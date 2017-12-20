60aff7aa7044ddae7a7528b7f898719a0f27b782
Ayush <ayushg.swatcat@gmail.com>
2017-12-20 22:08:42 +0530

    Edited DailyDietForm
    Problem in javascript.
    I dont know much js.
    Working fine without it.
    Can leave it like this, and associate the form input with date instead of adding rows.

--- Forms/Nachi/DailyDietForm.php
+++ Forms/Nachi/DailyDietForm.php
@@ -7,6 +7,7 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
+<!--
 	<script language="javascript">
 		function addRow() {
 			console.log("1");
@@ -144,6 +145,10 @@
 			}
 		}
 	</script>
+
+-->
+
+
 </head>
 
 <div class="container">
@@ -179,113 +184,76 @@
 		</div>
 	</div>
 	<hr>
-	<div class="row">
-		<table class="table table-bordered table-responsive" name="dailydietform" id="dailydietform">
+	<div class="row form-group form-check">
+		<table class="table table-bordered table-condensed small" name="dailydietform" id="dailydietform">
 			<thead>
+
 				<tr>
-					<th colspan="3" rowspan="2" width="5%">
-						<div class="col-md-4 text-center"><label class="control-label" for="date">Date:</label></div>
-						<div class="col-md-6">
+					<th colspan="3" rowspan="2">
+						<div class="form-inline text-">
+							<label>Date:</label>
 							<input type="date" class="form-control" name="date" id="date">
 						</div>
 					</th>
+
 					<th colspan="10" class="text-center">RTF/Liquid</th>
-					<th rowspan="2"></th>
+					<th rowspan="3">Remarks</th>
 				</tr>
+
 				<tr>
-					<th>
-						S-1<br>
-						(6 AM)
-					</th>
-					<th>
-						S-2<br>
-						(8 AM)
-					</th>
-					<th>
-						S-3<br>
-						(10 AM)
-					</th>
-					<th>
-						S-4<br>
-						(12 PM)
-					</th>
-					<th>
-						S-5<br>
-						(2 PM)
-					</th>
-					<th>
-						S-6<br>
-						(4 PM)
-					</th>
-					<th>
-						S-7<br>
-						(6 PM)
-					</th>
-					<th>
-						S-8<br>
-						(8 PM)
-					</th>
-					<th>
-						S-9<br>
-						(10 PM)
-					</th>
-					<th>
-						S-10<br>
-						(12 AM)
-					</th>
+					<th class="text-center">S-1 (6 AM)</th>
+					<th class="text-center">S-2 (8 AM)</th>
+					<th class="text-center">S-3 (10 AM)</th>
+					<th class="text-center">S-4 (12 PM)</th>
+					<th class="text-center">S-5 (2 PM)</th>
+					<th class="text-center">S-6 (4 PM)</th>
+					<th class="text-center">S-7 (6 PM)</th>
+					<th class="text-center">S-8 (8 PM)</th>
+					<th class="text-center">S-9 (10 PM)</th>
+					<th class="text-center">S-10 (12 AM)</th>
 				</tr>
+
 				<tr>
 					<th class="text-center">Room. No.</th>
-					<th class="text-center" width="15%">Patient Name & UHID No.</th>
+					<th class="text-center">Patient Name & UHID No.</th>
 					<th class="text-center">Diet Suggested By (Consultant/RMO)</th>
-					<th class="text-center">
-						M.Tea<br>
-						(6.30 AM)
-					</th>
-					<th class="text-center">
-						M.B/F<br>
-						(9 AM)
-					</th>
-					<th class="text-center">
-						Soup<br>
-						(11.30 AM)
-					</th>
-					<th class="text-center">
-						Lunch<br>
-						(12.30 PM)
-					</th>
-					<th class="text-center">
-
-					</th>
-					<th class="text-center">
-						E.Snacks<br>
-						(3.30 PM)
-					</th>
-					<th class="text-center">
-						Juice<br>
-						(5 PM)
-					</th>
-					<th class="text-center">
-						Dinner<br>
-						(7.30 PM)
-					</th>
-					<th class="text-center">
-						Milk<br>
-						(9.30 PM)
-					</th>
-					<th class="text-center">
-
-					</th>
-					<th class="text-center" width="15%">Remarks</th>
+					<th class="text-center">M.Tea (6.30 AM)</th>
+					<th class="text-center">M.B/F (9 AM)</th>
+					<th class="text-center">Soup (11.30 AM)</th>
+					<th class="text-center">Lunch (12.30 PM)</th>
+					<th></th>
+					<th class="text-center">E.Snacks (3.30 PM)</th>
+					<th class="text-center">Juice (5 PM)</th>
+					<th class="text-center">Dinner (7.30 PM)</th>
+					<th class="text-center">Milk (9.30 PM)</th>
+					<th class="text-center" />
 				</tr>
 			</thead>
+
 			<tbody>
-				<tr></tr>
+				<tr>
+					<td><input class="form-control" type="text" name="room_no" /></td>
+					<td><input class="form-control" type="text" name="patient_name" /></td>
+					<td><input class="form-control" type="text" name="diet_suggested_by"/></td>
+					<td class="text-center"><input class="form-check-input" type="checkbox" name="m_tea" /></td>
+					<td class="text-center"><input class="form-check-input" type="checkbox" name="m_breakfast" /></td>
+					<td class="text-center"><input class="form-check-input" type="checkbox" name="soup" /></td>
+					<td class="text-center"><input class="form-check-input" type="checkbox" name="lunch" /></td>
+					<td></td>
+					<td class="text-center"><input class="form-check-input" type="checkbox" name="e_snacks" /></td>
+					<td class="text-center"><input class="form-check-input" type="checkbox" name="juice" /></td>
+					<td class="text-center"><input class="form-check-input" type="checkbox" name="dinner" /></td>
+					<td class="text-center"><input class="form-check-input" type="checkbox" name="milk" /></td>
+					<td></td>
+					<td><input class="form-control" type="text" name="remarks" /></td>
+				</tr>
 			</tbody>
 		</table>
+		<!--
 		<div class="text-center">
 			<input type="button" name="addrow" onclick="addRow()" class="btn" value="Add A Row">
 		</div>
+	-->
 	</div>
 	</form>
 </div>
