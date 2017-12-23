2b518d9e93ab12e9e60fd5d05d812c705fbd7a05
Ayush <ayushg.swatcat@gmail.com>
2017-12-23 14:14:38 +0530

    LabSheet
    done

--- Forms/Stuti/lab_sheet.php
+++ Forms/Stuti/lab_sheet.php
@@ -3,269 +3,114 @@
 	<title>
 		Lab Sheet
 	</title>
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">	
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
 <div class="container">
 	<div class="page-header">
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-8">
 			<h1>Lab Sheet</h1>
-			</div>	
-			<div class="col-md-6">
-				<div class="text-left">
+			</div>
+			<div class="col-md-4">
+				<div class="text-right">
 					DOC NO. FMT/HIC/09 <br>
 					REV. No. 0.1 <br>
 					WEF 10-10-2015
-				</div>	
-			</div>
-			<div class="col-md-2 text-right">
-				<textarea class="text-right" id="Addressograph" rows="3" ></textarea>
-			</div>
-			<div class = "col-md-6">
-				<div class = "text-right"> 
-					Date: <input type = "date" id = "date"> &nbsp &nbsp Room No:<input type = "text" id = "room_no">
 				</div>
 			</div>
 		</div>
 	</div>
-	<form id="labsheet">
-		
+	<form name="labsheet">
+		<div class="row form-group">
+			<div class="col-md-10">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="col-md-6">
+							<label>Date : </label>
+						</div>
+						<div class="col-md-6">
+							<input class="form-control" type="date" name="date" />
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="col-md-6">
+							<label>Room No : </label>
+						</div>
+						<div class="col-md-6">
+							<input class="form-control" type="text" name="room_no" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2">
+				<textarea class="form-control" name="addressograph" placeholder="Addressograph"></textarea>
+			</div>
+		</div>
+
+		<hr />
+
 		<div class="row">
-		  <table class="table table-bordered">
+		  <table class="table table-bordered table-condensed">
 		    <thead>
-			  <tr>
-				<th>Sr No.</th>
-				<th>INVESTIGATION</th>
-				<th>DATE</th>
-				<th>NAME & SIGN</th>
-				<th>SIGN</th>
-				<th>REMARK</th>
-				
-			</tr>
+				  <tr>
+						<th class="text-center">Sr No.</th>
+						<th class="text-center">Investigation</th>
+						<th class="text-center">Date</th>
+						<th class="text-center">Name</th>
+						<th class="text-center">REMARK</th>
+					</tr>
 			 </thead>
-			<tbody>
-			  <tr>
-				   
-				<td><input type = "text" id = "1-srno"></td>
-				<td><input type = "text" id = "1-investigation"></td>
-				<td><input type = "date" id = "1-date"></td>
-				<td><input type = "text" id = "1-namesign"></td>
-				<td><input type = "text" id = "1-sign"></td>
-				<td><input type = "text" id = "1-remark"></td>
-				
-				
-			  </tr>
-				
-			<tr>
-				   
-				<td><input type = "text" id = "2-srno"></td>
-				<td><input type = "text" id = "2-investigation"></td>
-				<td><input type = "date" id = "2-date"></td>
-				<td><input type = "text" id = "2-namesign"></td>
-				<td><input type = "text" id = "2-sign"></td>
-				<td><input type = "text" id = "2-remark"></td>
-				
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "3-srno"></td>
-				<td><input type = "text" id = "3-investigation"></td>
-				<td><input type = "date" id = "3-date"></td>
-				<td><input type = "text" id = "3-namesign"></td>
-				<td><input type = "text" id = "3-sign"></td>
-				<td><input type = "text" id = "3-remark"></td>
-				
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "4-srno"></td>
-				<td><input type = "text" id = "4-investigation"></td>
-				<td><input type = "date" id = "4-date"></td>
-				<td><input type = "text" id = "4-namesign"></td>
-				<td><input type = "text" id = "4-sign"></td>
-				<td><input type = "text" id = "4-remark"></td>
-				
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "5-srno"></td>
-				<td><input type = "text" id = "5-investigation"></td>
-				<td><input type = "date" id = "5-date"></td>
-				<td><input type = "text" id = "5-namesign"></td>
-				<td><input type = "text" id = "5-sign"></td>
-				<td><input type = "text" id = "5-remark"></td>
-			
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "6-srno"></td>
-				<td><input type = "text" id = "6-investigation"></td>
-				<td><input type = "date" id = "6-date"></td>
-				<td><input type = "text" id = "6-namesign"></td>
-				<td><input type = "text" id = "7-sign"></td>
-				<td><input type = "text" id = "7-remark"></td>
-				
-				
-			  </tr>
-			  
-			  <tr>
-				<td><input type = "text" id = "8-srno"></td>
-				<td><input type = "text" id = "8-investigation"></td>
-				<td><input type = "date" id = "8-date"></td>
-				<td><input type = "text" id = "8-namesign"></td>
-				<td><input type = "text" id = "8-sign"></td>
-				<td><input type = "text" id = "8-remark"></td>
-				
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "9-srno"></td>
-				<td><input type = "text" id = "9-investigation"></td>
-				<td><input type = "date" id = "9-date"></td>
-				<td><input type = "text" id = "9-namesign"></td>
-				<td><input type = "text" id = "9-sign"></td>
-				<td><input type = "text" id = "9-remark"></td>
-				
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "10-srno"></td>
-				<td><input type = "text" id = "10-investigation"></td>
-				<td><input type = "date" id = "10-date"></td>
-				<td><input type = "text" id = "10-namesign"></td>
-				<td><input type = "text" id = "10-sign"></td>
-				<td><input type = "text" id = "10-remark"></td>
-				
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "11-srno"></td>
-				<td><input type = "text" id = "11-investigation"></td>
-				<td><input type = "date" id = "11-date"></td>
-				<td><input type = "text" id = "11-namesign"></td>
-				<td><input type = "text" id = "11-sign"></td>
-				<td><input type = "text" id = "11-remark"></td>
-				
-			  </tr>
-			  
-			  <tr>
-				<td><input type = "text" id = "12-srno"></td>
-				<td><input type = "text" id = "12-investigation"></td>
-				<td><input type = "date" id = "12-date"></td>
-				<td><input type = "text" id = "12-namesign"></td>
-				<td><input type = "text" id = "12-sign"></td>
-				<td><input type = "text" id = "12-remark"></td>
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "13-srno"></td>
-				<td><input type = "text" id = "13-investigation"></td>
-				<td><input type = "date" id = "13-date"></td>
-				<td><input type = "text" id = "13-namesign"></td>
-				<td><input type = "text" id = "13-sign"></td>
-				<td><input type = "text" id = "13-remark"></td>
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "14-srno"></td>
-				<td><input type = "text" id = "14-investigation"></td>
-				<td><input type = "date" id = "14-date"></td>
-				<td><input type = "text" id = "14-namesign"></td>
-				<td><input type = "text" id = "14-sign"></td>
-				<td><input type = "text" id = "14-remark"></td>
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "15-srno"></td>
-				<td><input type = "text" id = "15-investigation"></td>
-				<td><input type = "date" id = "15-date"></td>
-				<td><input type = "text" id = "15-namesign"></td>
-				<td><input type = "text" id = "15-sign"></td>
-				<td><input type = "text" id = "15-remark"></td>
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "16-srno"></td>
-				<td><input type = "text" id = "16-investigation"></td>
-				<td><input type = "date" id = "16-date"></td>
-				<td><input type = "text" id = "16-namesign"></td>
-				<td><input type = "text" id = "16-sign"></td>
-				<td><input type = "text" id = "16-remark"></td>
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "17-srno"></td>
-				<td><input type = "text" id = "17-investigation"></td>
-				<td><input type = "date" id = "17-date"></td>
-				<td><input type = "text" id = "17-namesign"></td>
-				<td><input type = "text" id = "17-sign"></td>
-				<td><input type = "text" id = "17-remark"></td>
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "18-srno"></td>
-				<td><input type = "text" id = "18-investigation"></td>
-				<td><input type = "date" id = "18-date"></td>
-				<td><input type = "text" id = "18-namesign"></td>
-				<td><input type = "text" id = "18-sign"></td>
-				<td><input type = "text" id = "18-remark"></td>
-				
-			  </tr>
-			  
-			  <tr>
-				   
-				<td><input type = "text" id = "19-srno"></td>
-				<td><input type = "text" id = "19-investigation"></td>
-				<td><input type = "date" id = "19-date"></td>
-				<td><input type = "text" id = "19-namesign"></td>
-				<td><input type = "text" id = "19-sign"></td>
-				<td><input type = "text" id = "19-remark"></td>
-				
-			  </tr>
-			  
-			   <tr>
-				   
-				<td><input type = "text" id = "20-srno"></td>
-				<td><input type = "text" id = "20-investigation"></td>
-				<td><input type = "date" id = "20-date"></td>
-				<td><input type = "text" id = "20-namesign"></td>
-				<td><input type = "text" id = "20-sign"></td>
-				<td><input type = "text" id = "20-remark"></td>
-				
-			  </tr>
-			  
+				<tbody>
+				  <tr>
+						<td>1</td>
+						<td><input class="form-control" type = "text" id = "investigation_01"></td>
+						<td><input class="form-control" type = "date" id = "date_01"></td>
+						<td><input class="form-control" type = "text" id = "name_01"></td>
+						<td><input class="form-control" type = "text" id = "remark_01"></td>
+				  </tr>
+					<tr>
+						<td>2</td>
+						<td><input class="form-control" type = "text" id = "investigation_02"></td>
+						<td><input class="form-control" type = "date" id = "date_02"></td>
+						<td><input class="form-control" type = "text" id = "name_02"></td>
+						<td><input class="form-control" type = "text" id = "remark_02"></td>
+				  </tr>
+					<tr>
+						<td>3</td>
+						<td><input class="form-control" type = "text" id = "investigation_03"></td>
+						<td><input class="form-control" type = "date" id = "date_03"></td>
+						<td><input class="form-control" type = "text" id = "name_03"></td>
+						<td><input class="form-control" type = "text" id = "remark_03"></td>
+				  </tr>
+					<tr>
+						<td>4</td>
+						<td><input class="form-control" type = "text" id = "investigation_04"></td>
+						<td><input class="form-control" type = "date" id = "date_04"></td>
+						<td><input class="form-control" type = "text" id = "name_04"></td>
+						<td><input class="form-control" type = "text" id = "remark_04"></td>
+				  </tr>
+					<tr>
+						<td>6</td>
+						<td><input class="form-control" type = "text" id = "investigation_06"></td>
+						<td><input class="form-control" type = "date" id = "date_06"></td>
+						<td><input class="form-control" type = "text" id = "name_06"></td>
+						<td><input class="form-control" type = "text" id = "remark_06"></td>
+				  </tr>
+					<tr>
+						<td>7</td>
+						<td><input class="form-control" type = "text" id = "investigation_07"></td>
+						<td><input class="form-control" type = "date" id = "date_07"></td>
+						<td><input class="form-control" type = "text" id = "name_07"></td>
+						<td><input class="form-control" type = "text" id = "remark_07"></td>
+				  </tr>
 			  </tbody>
 		  </table>
 		</div>
-	
 	</form>
 </div>
-<div class = "col-md-6">
-	Sign of RMO 
-	
-</div>
-<div class = "text-right">Sign of Staff</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</body>
+</html>
